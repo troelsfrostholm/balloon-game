d015cef3ac7697b7c8648890f6c3f11ff9da96f7
Troels Frostholm Mogensen <troelsfrostholm@gmail.com>
2010-11-14 21:52:39 +0100

    Removed dead code from util and style sheet

--- game/gamelib/util.js
+++ game/gamelib/util.js
@@ -8,22 +8,13 @@ function require_input(input, type, message)
     return true;
 }
 
-//this function is destructive on value. 
-//It should be changed to be non-destructive
-function inject(array, value, closure) {
-   for(i in array) {
-	value = closure(value, array[i]);
-    }
-    return value;
-}
-
-function prototype_classof(object)
+/*function prototype_classof(object)
 {
     var getType = Object.prototype.toString;
     prototypeString =  getType.apply(object);
     classname = prototypeString.match(/\[object ([a-zA-Z]*)\]/)[1];
     return classname;
-}
+    }*/
 
 /* Returns the class name of the argument or undefined if
    it's not a valid JavaScript object.
@@ -40,21 +31,13 @@ function classof(obj) {
     return undefined;
 };
 
-function onLoadImage(image, callback, pollFrequency)
-{
-    if(!pollFrequency) pollFrequency=100;
-    if(!image.width || !image.height) {
-	setTimeout(onLoadImage, pollFrequency);
-    }
-    return callback();
-}
-
+/*
 function loadScript(filename)
 {
     var fileref=document.createElement('script');
     fileref.setAttribute("type","text/javascript");
     fileref.setAttribute("src", filename);
-}
+    }*/
 
 function createImage(filename)
 {

263a68aeae000fb5a5d3c4d07431fcc3046eaeb8
jenl <jenl@itu.dk>
2010-12-01 16:38:06 +0100

    fixed impassableToBalloon

--- game/item-behaviours.js
+++ game/item-behaviours.js
@@ -17,26 +17,30 @@ Behaviours.dieWhenFarAway = function(obj)
     }
 }
 
-Behaviours.impassableToBalloon = function(obj)
-{
+function impassableToBalloon(obj)
+{    
 	// Only do something if there is a collision.
 	if (obj.getBoundingBox().collidesWith( balloon.getBoundingBox() ))
 	{
+        var bX = balloon.pos[0].x;
+        var bY = balloon.pos[0].y;
 		var bw = balloon.getBoundingBox().width/2;
-		var bh =  balloon.getBoundingBox().height/2;
+		var bh = balloon.getBoundingBox().height/2;
 		var ow = obj.getBoundingBox().width/2;
 		var oh = obj.getBoundingBox().height/2;
 
-		balloonTopLeft = new Point(balloon.pos[0].x - bw , balloon.pos[0].y - bh);
-		balloonTopRight = new Point(balloon.pos[0].x + bw , balloon.pos[0].y - bh);
-		balloonBottomLeft = new Point(balloon.pos[0].x - bw , balloon.pos[0].y + bh);
-		balloonBottomRight = new Point(balloon.pos[0].x + bw , balloon.pos[0].y + bh);
+		balloonTopLeft = new Point(bX - bw , bY - bh);
+        balloonCentreLeft = new Point(bX - bw , bY);
+		balloonBottomLeft = new Point(bX - bw , bY + bh);
+		balloonTopRight = new Point(bX + bw , bY - bh);
+        balloonCentreRight = new Point(bX + bw , bY);
+		balloonBottomRight = new Point(bX + bw , bY + bh);
 		objTopLeft = new Point(obj.pos[0].x - ow, obj.pos[0].y - oh);
 		objTopRight = new Point(obj.pos[0].x + ow, obj.pos[0].y - oh);
 		objBottomLeft = new Point(obj.pos[0].x - ow, obj.pos[0].y + oh);
 		objBottomRight = new Point(obj.pos[0].x + ow, obj.pos[0].y + oh);
 		
-		corners = new Array(balloonTopLeft, balloonTopRight, balloonBottomLeft, balloonBottomRight);
+		corners = new Array(balloonCentreLeft , balloonCentreRight , balloonBottomLeft, balloonBottomRight , balloonTopLeft, balloonTopRight);
 
 		// One of the balloon's corners will always collide if a collision is detected.
 		for (var i in corners)
@@ -44,15 +48,18 @@ Behaviours.impassableToBalloon = function(obj)
 			// Check for collission with current corner
 			if ( obj.getBoundingBox().collidesWithPoint(corners[i]) == true)
 			{
+                var c2b = corners[i].sub(obj.pos[0]);
 				// If collission is in object's UPPER half
 				if (corners[i].y < obj.pos[0].y)
 				{
 					// Collission in object's LEFT half
 					if (corners[i].x < obj.pos[0].x)
 					{
+                        var c2c = objTopLeft.sub(obj.pos[0]);
 						// Bounce off LEFT SIDE
-						if (corners[i].angle2(obj.pos[0]) > objTopLeft.angle2(obj.pos[0]))					
+						if ( c2b.y/c2b.x < c2c.y/c2c.x )
 						{
+                            console.log("corner " + i + " hit UPPER LEFT SIDE");
 							balloon.pos[0].x = objTopLeft.x - bw ;
 
 							if ( balloon.pos[1].x > 0)
@@ -61,6 +68,7 @@ Behaviours.impassableToBalloon = function(obj)
 						// Bounce off TOP
 						else
 						{
+                            console.log("corner " + i + " hit TOP LEFT");
 							balloon.pos[0].y = objTopLeft.y - bh ;
 
 							if ( balloon.pos[1].y > 0)
@@ -70,9 +78,11 @@ Behaviours.impassableToBalloon = function(obj)
 					// Collission in object's RIGHT half
 					else
 					{
-						// Bounce off LEFT SIDE
-						if (corners[i].angle(obj.pos[0]) > objTopRight.angle(obj.pos[0]))					
+                        var c2c = objTopRight.sub(obj.pos[0]);
+						// Bounce off RIGHT SIDE
+						if ( c2b.y/c2b.x > c2c.y/c2c.x )
 						{
+                            console.log("corner " + i + " hit UPPER RIGHT SIDE");
 							balloon.pos[0].x = objTopRight.x + bw ;
 
 							if ( balloon.pos[1].x < 0)
@@ -81,6 +91,7 @@ Behaviours.impassableToBalloon = function(obj)
 						else
 						// Bounce off TOP
 						{
+                            console.log("corner " + i + " hit TOP RIGHT");
 							balloon.pos[0].y = objTopLeft.y - bh ;
 
 							if ( balloon.pos[1].y > 0)
@@ -93,9 +104,11 @@ Behaviours.impassableToBalloon = function(obj)
 				{
 					if (corners[i].x < obj.pos[0].x)
 					{
+                        var c2c = objBottomLeft.sub(obj.pos[0]);
 						// Bounce off LEFT SIDE
-						if (corners[i].angle(obj.pos[0]) < objBottomLeft.angle(obj.pos[0]))					
+						if ( c2b.y/c2b.x > c2c.y/c2c.x )
 						{
+                            console.log("corner " + i + " hit LOWER LEFT SIDE");
 							balloon.pos[0].x = objBottomLeft.x - bw ;
 
 							if ( balloon.pos[1].x > 0)
@@ -104,6 +117,7 @@ Behaviours.impassableToBalloon = function(obj)
 						else
 						// Bounce off BOTTOM
 						{
+                            console.log("corner " + i + " hit BOTTOM LEFT");
 							balloon.pos[0].y = objBottomLeft.y + bh ;
 
 							if ( balloon.pos[1].y < 0)
@@ -112,9 +126,11 @@ Behaviours.impassableToBalloon = function(obj)
 					}
 					else
 					{
+                        var c2c = objBottomRight.sub(obj.pos[0]);
 						// Bounce off RIGHT SIDE
-						if (corners[i].angle(obj.pos[0]) > objBottomRight.angle(obj.pos[0]))					
+						if ( c2b.y/c2b.x < c2c.y/c2c.x )
 						{
+                            console.log("corner " + i + " hit LOWER RIGHT SIDE");
 							balloon.pos[0].x = objBottomRight.x + bw ;
 
 							if ( balloon.pos[1].x < 0)
@@ -123,9 +139,10 @@ Behaviours.impassableToBalloon = function(obj)
 						else
 						// Bounce off BOTTOM
 						{
-							balloon.pos[0].y = objBottomLeft.y + bh ;
+                            console.log("corner " + i + " hit BOTTOM RIGHT");
+							balloon.pos[0].y = objBottomRight.y + bh ;
 
-							if ( balloon.pos[1].y > 0)
+							if ( balloon.pos[1].y < 0)
 								balloon.pos[1].y *= -1;
 						}
 					}

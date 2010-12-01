2fc3119508b2051877ed1ba329395ccba94f41e0
jenl <jenl@itu.dk>
2010-12-01 13:36:29 +0100

    Fixed impassableToBalloon behaviour

--- game/item-behaviours.js
+++ game/item-behaviours.js
@@ -19,23 +19,119 @@ Behaviours.dieWhenFarAway = function(obj)
 
 Behaviours.impassableToBalloon = function(obj)
 {
-	// if balloon collides with object
-	if (obj.getBoundingBox().collidesWith(balloon.getBoundingBox()))
+	// Only do something if there is a collision.
+	if (obj.getBoundingBox().collidesWith( balloon.getBoundingBox() ))
 	{
-		// balloon BELOW object
-		if ( (balloon.pos[0].y) > (obj.pos[0].y + obj.image.height/2) )
+		var bw = balloon.getBoundingBox().width/2;
+		var bh =  balloon.getBoundingBox().height/2;
+		var ow = obj.getBoundingBox().width/2;
+		var oh = obj.getBoundingBox().height/2;
+
+		balloonTopLeft = new Point(balloon.pos[0].x - bw , balloon.pos[0].y - bh);
+		balloonTopRight = new Point(balloon.pos[0].x + bw , balloon.pos[0].y - bh);
+		balloonBottomLeft = new Point(balloon.pos[0].x - bw , balloon.pos[0].y + bh);
+		balloonBottomRight = new Point(balloon.pos[0].x + bw , balloon.pos[0].y + bh);
+		objTopLeft = new Point(obj.pos[0].x - ow, obj.pos[0].y - oh);
+		objTopRight = new Point(obj.pos[0].x + ow, obj.pos[0].y - oh);
+		objBottomLeft = new Point(obj.pos[0].x - ow, obj.pos[0].y + oh);
+		objBottomRight = new Point(obj.pos[0].x + ow, obj.pos[0].y + oh);
+		
+		corners = new Array(balloonTopLeft, balloonTopRight, balloonBottomLeft, balloonBottomRight);
+
+		// One of the balloon's corners will always collide if a collision is detected.
+		for (var i in corners)
 		{
-			balloon.pos[1].y *= -0.95;
-		}
-		// balloon RIGHT of object
-		else if ( balloon.pos[0].x > (obj.pos[0].x + obj.image.width/2) )
-		{
-			balloon.pos[1].x *= -0.95;
-		}
-		// balloon LEFT of object
-		else if ( balloon.pos[0].x < (obj.pos[0].x - obj.image.width/2) )
-		{
-			balloon.pos[1].x *= -0.95;
-		}
+			// Check for collission with current corner
+			if ( obj.getBoundingBox().collidesWithPoint(corners[i]) == true)
+			{
+				// If collission is in object's UPPER half
+				if (corners[i].y < obj.pos[0].y)
+				{
+					// Collission in object's LEFT half
+					if (corners[i].x < obj.pos[0].x)
+					{
+						// Bounce off LEFT SIDE
+						if (corners[i].angle2(obj.pos[0]) > objTopLeft.angle2(obj.pos[0]))					
+						{
+							balloon.pos[0].x = objTopLeft.x - bw ;
+
+							if ( balloon.pos[1].x > 0)
+								balloon.pos[1].x *= -1;
+						}
+						// Bounce off TOP
+						else
+						{
+							balloon.pos[0].y = objTopLeft.y - bh ;
+
+							if ( balloon.pos[1].y > 0)
+								balloon.pos[1].y *= -1;
+						}
+					}
+					// Collission in object's RIGHT half
+					else
+					{
+						// Bounce off LEFT SIDE
+						if (corners[i].angle(obj.pos[0]) > objTopRight.angle(obj.pos[0]))					
+						{
+							balloon.pos[0].x = objTopRight.x + bw ;
+
+							if ( balloon.pos[1].x < 0)
+								balloon.pos[1].x *= -1;
+						}
+						else
+						// Bounce off TOP
+						{
+							balloon.pos[0].y = objTopLeft.y - bh ;
+
+							if ( balloon.pos[1].y > 0)
+								balloon.pos[1].y *= -1;
+						}
+					}
+				}
+				// BOTTOM half
+				else
+				{
+					if (corners[i].x < obj.pos[0].x)
+					{
+						// Bounce off LEFT SIDE
+						if (corners[i].angle(obj.pos[0]) < objBottomLeft.angle(obj.pos[0]))					
+						{
+							balloon.pos[0].x = objBottomLeft.x - bw ;
+
+							if ( balloon.pos[1].x > 0)
+								balloon.pos[1].x *= -1;
+						}
+						else
+						// Bounce off BOTTOM
+						{
+							balloon.pos[0].y = objBottomLeft.y + bh ;
+
+							if ( balloon.pos[1].y < 0)
+								balloon.pos[1].y *= -1;
+						}
+					}
+					else
+					{
+						// Bounce off RIGHT SIDE
+						if (corners[i].angle(obj.pos[0]) > objBottomRight.angle(obj.pos[0]))					
+						{
+							balloon.pos[0].x = objBottomRight.x + bw ;
+
+							if ( balloon.pos[1].x < 0)
+								balloon.pos[1].x *= -1;
+						}
+						else
+						// Bounce off BOTTOM
+						{
+							balloon.pos[0].y = objBottomLeft.y + bh ;
+
+							if ( balloon.pos[1].y > 0)
+								balloon.pos[1].y *= -1;
+						}
+					}
+				}
+				break;
+			}
+		}		
 	}
 }

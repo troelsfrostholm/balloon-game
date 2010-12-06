1e02b2cf55e73e01329501dccf5c8ecd5644ce34
jenl <jenl@itu.dk>
2010-12-06 13:20:12 +0100

    Added wind behaviours

--- game/enemy-behaviours.js
+++ game/enemy-behaviours.js
@@ -45,3 +45,27 @@ Behaviours.falling = function(obj)
 {
     obj.acc(0, 0.5);
 }
+
+
+Behaviours.wind = function(obj)
+{
+    if (Math.abs(obj.pos[0].x - balloon.pos[0].x) + Math.abs(obj.pos[0].y - balloon.pos[0].y) < 400)
+    {
+        blowHardAtBalloon(obj.pos[0]);
+    }
+}
+
+Behaviours.blowHardAtBalloon = function(obj)
+{
+    balloon.pos[2] = balloon.pos[2].add(pushForce(obj.pos[0]));
+    balloon.pos[2].x *= 1.5;
+    balloon.pos[2].y *= 1.5;
+}
+
+Behaviours.verticalPatrolling = function(obj)
+{
+    if (obj.pos[0].x > 1400 || obj.pos[0].x < -1400)
+    {
+        obj.pos[1].x *= -1;
+    }
+}
